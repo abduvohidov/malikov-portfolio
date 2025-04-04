836ba788a8c5bb8d9e2ba5247b6f4de83c1e4d5e
Abdullah <abduvohidov.abdullokh@gmail.com>
2025-04-05 03:46:39 +0500

    fix: priceCard

--- src/features/PriceCard/ui/PriceCard.tsx
+++ src/features/PriceCard/ui/PriceCard.tsx
@@ -5,32 +5,22 @@ import React from "react";
 
 interface PriceCardProps {
   className?: string;
-  tarifName: string | string[] |string[][];
-  contents: string | string[] | string[][];
+  tarifName: string;
+  contents: string[];
   price: { from: number; to: number };
 }
 
 export const PriceCard: React.FC<PriceCardProps> = (props) => {
   const { className, tarifName, contents, price } = props;
 
-  // If tarifName is an array, join it into a single string
-  const tarifNameDisplay = Array.isArray(tarifName)
-    ? tarifName.join(", ")
-    : tarifName;
-
-  // Normalize contents into a flat array
-  const contentsArray = Array.isArray(contents)
-    ? contents.flat() // Flatten nested arrays, if any
-    : [contents]; // Wrap in an array if it's a single string
-
   return (
     <Card className={cn(className, "relative min-w-[300px]")}>
       <CardHeader>
-        <Text variant="display-1">{tarifNameDisplay}</Text>
+        <Text variant="display-1">{tarifName}</Text>
       </CardHeader>
       <CardContent className="mb-12">
         <ul className="text-gray-300">
-          {contentsArray.map((item, index) => (
+          {contents.map((item, index) => (
             <li key={index}>- {item}</li>
           ))}
         </ul>
